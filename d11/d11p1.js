f7f6a33247145c272838100d5669a5ac582546a4
momoxstarx <momo.star@gmail.com>
2022-12-19 23:12:18 -0800

    d11 wip

--- d11/d11p1.js
+++ d11/d11p1.js
@@ -18,50 +18,62 @@ class Monkey {
   }
 }
 
-let monkeys = [];
-let monkey;
-let id = 0;
+let monkeys = buildInstructions()
+simulateRound(monkeys);
 
-for (let line of series) {
-  if (line.startsWith("monkey")) {
-    monkey = new Monkey();
-    monkey.id = line.match(/monkey (\d):$/)[1];
-    console.log("monkey " + monkey.id);
-    monkeys.push(monkey);
-  } else {
-    monkey = monkeys[id];
-    if (line.trim().startsWith("starting items")) {
-      // splits as str, need to change to int during op
-      let temp = line.split("starting items: ")[1].split(",");
-      for (let num of temp) {
-        monkey.startItems.push(parseInt(num.trim()));
+function buildInstructions() {
+  let monkeys = [];
+
+  for (let i = 0; i < series.length; i += 7) {
+    if (series[i].startsWith("monkey")) {
+      let monkey = new Monkey();
+      monkey.id = parseInt(series[i].match(/monkey (\d):$/)[1]);
+      if (series[i + 1].trim().startsWith("starting items")) {
+        // splits as str, need to change to int during op
+        let temp = series[i + 1].split("starting items: ")[1].split(",");
+        for (let num of temp) {
+          monkey.startItems.push(parseInt(num.trim()));
+        }
       }
-      console.log(monkey.startItems);
+      if (series[i + 2].trim().startsWith("operation")) {
+        // run needs to eval with var 'old'
+        monkey.operation = series[i + 2]
+          .split("operation: ")[1]
+          .replace("new = ", " ");
+      }
+      if (series[i + 3].trim().startsWith("test")) {
+        monkey.test = series[i + 3]
+          .split("test: ")[1]
+          .replace("divisible by", "/");
+      }
+      if (series[i + 4].trim().startsWith("if true")) {
+        monkey.true = parseInt(series[i + 4].match(/(\d)$/)[1]);
+      }
+      if (series[i + 5].trim().startsWith("if false")) {
+        monkey.false = parseInt(series[i + 5].match(/(\d)$/)[1]);
+        monkey.done = true;
+      }
+      if (monkey.done) {
+        monkeys.push(monkey);
+      }
     }
-    if (line.trim().startsWith("operation")) {
-      // run needs to eval with var 'old'
-      monkey.operation = line
-        .split("operation: ")[1]
-        .replace("new = ", "return ");
-      console.log(monkey.operation);
-    }
-    if (line.trim().startsWith("test")) {
-      monkey.test = line.split("test: ")[1].replace("divisible by", "/");
-      console.log(monkey.test);
-    }
-    if (line.trim().startsWith("if true")) {
-      monkey.true = line.match(/(\d)$/)[1];
-      console.log(monkey.true);
-    }
-    if (line.trim().startsWith("if false")) {
-      monkey.false = line.match(/(\d)$/)[1];
-      console.log(monkey.false);
-      monkey.done = true;
-    }
-    if (monkey.done) {
-      monkeys[id] = monkey;
-      console.log(monkeys);
-      id++;
+  }
+  return monkeys
+}
+
+function simulateRound(monkeys) {
+  for (let monkey of monkeys) {
+    // add inspect
+    monkey.inspect += monkey.startItems.length
+    // calculate new worrylevel
+    // for (let old of monkey.startItems) {
+    while (monkey.startItems.length > 0) {
+      let old = monkey.startItems.shift()
+      let newWorry = eval(monkey.operation)
+      console.log(newWorry)
+
+      // throw
+      
     }
   }
 }
