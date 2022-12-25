4d30fd79c301c7bf2c365aaddeeebdbd5ad669c5
Monica Hong <momo.star@gmail.com>
2022-12-24 23:13:31 -0800

    laptop changes

---
+++ d12p1.js
@@ -0,0 +1,39 @@
+var fs = require("fs");
+var text = fs.readFileSync("./d12/d12-inputs2.txt", "utf-8");
+var lines = text.split("\r\n");
+
+// Hill Climbing Algorithm
+/*
+height map
+'S' (height < a) is starting point - 'E' (height > z) is where we want to go
+can only move 1 square up, down, left or right
+next square can only be 1 step taller
+few steps as possible to 'E'
+*/
+
+//how to organize?
+// [X] no need to parse instructions
+// [X] FIND S position
+// [X] establish dominance
+// [] how do I know how many times to deterine direction? --> recursive?
+//  - [] recursive like fibonacci algo and use logic to decide direction
+//  - []
+
+const abc = "SabcdefghijklmnopqrstuvwxyzE";
+const hierarchy = {};
+for (let i = 0; i < abc.length; i++) {
+  hierarchy[abc[i]] = i;
+}
+console.log(hierarchy);
+
+let S = findS();
+console.log(S);
+
+function findS() {
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes("S")) {
+      // console.log(`line [${i}][${lines[i].indexOf("S")}]`)
+      return [i, lines[i].indexOf("S")];
+    }
+  }
+}
